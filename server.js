b1b5cb668f47e4e905217e59098dfb9905b71f00
oyasumi <k.anuchit@kkumail.com>
2020-07-25 17:38:00 +0700

    Add module dotenv

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
+require('dotenv').config();
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT;
 
 const app = express();
 
@@ -15,7 +16,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
-  res.send("Hello");
+  res.send("Hello World");
 });
 
 app.listen(PORT, () => {
