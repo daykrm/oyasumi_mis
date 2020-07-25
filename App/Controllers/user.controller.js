ea433285f8bf7b577506f4999fae892a81f5e67e
oyasumi <k.anuchit@kkumail.com>
2020-07-25 20:05:42 +0700

    Update create method in user controller

--- App/Controllers/user.controller.js
+++ App/Controllers/user.controller.js
@@ -13,23 +13,33 @@ exports.create = (req, res) => {
     return;
   }
 
-  //Create new user
-  const user = new User({
-    name: { first: req.body.fname, last: req.body.lname },
-    username: req.body.username,
-    password: req.body.password,
-    role: req.body.role,
-  });
+  User.countDocuments({ username: req.body.username }, function (err, count) {
+    if (count > 0) {
+      //document exists });
+      res.status(400).send({
+        message: `This username : ${req.body.username} already taken!`,
+      });
+      return;
+    } else {
+      //Create new user
+      const user = new User({
+        name: { first: req.body.fname, last: req.body.lname },
+        username: req.body.username,
+        password: req.body.password,
+        role: req.body.role,
+      });
 
-  //Save to DB
-  user
-    .save(user)
-    .then((data) => {
-      res.send(data);
-    })
-    .catch((err) => {
-      console.log(err);
-    });
+      //Save to DB
+      user
+        .save(user)
+        .then((data) => {
+          res.send(data);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    }
+  });
 };
 
 exports.findAll = (req, res) => {
@@ -43,22 +53,15 @@ exports.findAll = (req, res) => {
 };
 
 exports.fineOne = (req, res) => {
-  User.findOne({ _id : req.params.id }, (err, user) => {
+  User.findOne({ _id: req.params.id }, (err, user) => {
     if (err) throw err;
 
     user.comparePassword("1234", function (err, isMatch) {
       if (err) throw err;
 
-      res.send({ message : `1234 : ${isMatch}`})
+      res.send({ message: `1234 : ${isMatch}` });
       console.log("1234:", isMatch); // -&gt; Password123: true
     });
-
-    user.comparePassword("123Password", function (err, isMatch) {
-      if (err) throw err;
-      
-      console.log("123Password:", isMatch); // -&gt; 123Password: false
-    });
-
   });
 };
 
