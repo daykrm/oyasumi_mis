7d38454da3d0d8dc10f5b10dc0e7dbb57aa02f44
oyasumi <k.anuchit@kkumail.com>
2020-07-29 16:20:52 +0700

    Setup Redux

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
+import { Provider } from "react-redux";
+import store from "./store";
+
+import Navbar from "./Components/Layout/Navbar";
+import Landing from "./Components/Layout/Landing";
+import Register from "./Components/Auth/Register";
+import Login from "./Components/Auth/Login";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider store={store}>
+      <Router>
+        <div className="App">
+          <Navbar />
+          <Route exact path="/" component={Landing} />
+          <Route exact path="/register" component={Register} />
+          <Route exact path="/login" component={Login} />
+        </div>
+      </Router>
+    </Provider>
   );
 }
 
